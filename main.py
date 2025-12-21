4d1e66d4a70cc7a965f1eebad014aacd6a175a62
Sguinza <cguinda00@gmail.com>
2025-12-21 19:03:16 +0100

    added optional exact match on the fields

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ DBPath = "C:\\temp\\"
 mtg_db = MtgDB(DBPath + "my_db.fs")
 window = Tk()
 window.title('MTG Tool Export')
-window.geometry('400x250')
+window.geometry('400x300')
 
 try:
     os.mkdir(directory_name)
@@ -45,6 +45,9 @@ def disable_widg():
     expansion_text['state'] = 'disabled'
     name_text['state'] = 'disabled'
     type_text['state'] = 'disabled'
+    checkbox_set['state'] = 'disabled'
+    checkbox_name['state'] = 'disabled'
+    checkbox_type['state'] = 'disabled'
 
 def enable_widg():
     
@@ -53,28 +56,40 @@ def enable_widg():
     expansion_text['state'] = 'normal'
     name_text['state'] = 'normal'
     type_text['state'] = 'normal'
+    checkbox_set['state'] = 'normal'
+    checkbox_name['state'] = 'normal'
+    checkbox_type['state'] = 'normal'
 
 def on_closing():
         #mtg_db.close()
         window.destroy()
 
 def export():
-    mtg_db.commit()
+
     cards = mtg_db.root.scryfall_cards
     progress_bar.pack()
     set_str = expansion_text.get()
     name_str = name_text.get()
     type_str = type_text.get()
+    set_value_bool = set_value.get()
+    name_value_bool = name_value.get()
+    type_value_bool = type_value.get()
+
 
     args = {'name' : name_str, 'set' : set_str, 'type_line' : type_str}
-    real_args = {}
+    condition = {'name' : name_value_bool, 'set' : set_value_bool, 'type_line' : type_value_bool}
+    where_set = {}
+    where_exactly_set = {}
 
     for (key, val) in args.items():
         if (val!='') :
-            real_args[key] = val
-        
+            if (condition[key] == False) :
+                where_set[key] = val
+            else :
+                where_exactly_set[key] = val    
     progress_bar.start()
-    mtg_set = cards.where_exactly(**real_args)
+    mtg_set = cards.where_exactly(**where_exactly_set)
+    mtg_set = mtg_set.where(**where_set)
     with open(directory_name + '\\estrazione.txt', 'w', encoding="utf-8") as f:
         f.write(mtg_set.pretty_print_str())        
     progress_bar.stop()
@@ -88,6 +103,14 @@ expansion_text = Entry()
 name_text = Entry()
 type_text = Entry()
 
+set_value = BooleanVar()
+name_value = BooleanVar()
+type_value = BooleanVar()
+
+checkbox_set = Checkbutton(text = 'exact match', variable=set_value, onvalue=True, offvalue=False)
+checkbox_name = Checkbutton(text = 'exact match', variable=name_value, onvalue=True, offvalue=False)
+checkbox_type = Checkbutton(text = 'exact match', variable=type_value, onvalue=True, offvalue=False)
+
 button_export = Button(text = "export")
 button_export.bind("<Button-1>", Export_exp)
 button_update_DB = Button(text = "Update")
@@ -100,10 +123,13 @@ type_label = Label(window, text='Type :')
 
 name_label.pack()
 name_text.pack()
+checkbox_name.pack()
 type_label.pack()
 type_text.pack()
+checkbox_type.pack()
 expansion_label.pack()
 expansion_text.pack()
+checkbox_set.pack()
 button_update_DB.pack()
 button_export.pack()
 
